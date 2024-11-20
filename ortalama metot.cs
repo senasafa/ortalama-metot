de05bcf7c0e19437d544d41688af3917e5df5c23
senasafa <senasafa8@gmail.com>
2024-11-20 23:50:07 +0300

    Add files via upload

---
+++ ortalama metot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+Console.Write("öğrenci adı: ");
+string ad = Console.ReadLine();
+
+Console.Write("öğrenci soyadı");
+string soyad = Console.ReadLine();
+
+Console.Write("1.sınav notunu girin: ");
+
+int not1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2.sınav notunu girin: ");
+int not2 = Convert.ToInt32(Console.ReadLine());
+
+double ortalama = (not1 + not2) / 2;
+ Console.WriteLine(ortalama);
